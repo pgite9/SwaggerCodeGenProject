1fae1552685412fd510af595d5243da3364041f3
pareshgite <gite.paresh@gmail.com>
2021-06-03 17:30:04 +0530

    add repo

---
+++ src/test/java/petstoretests/PetStoreUserTest.java
@@ -0,0 +1,13 @@
+package petstoretests;
+
+import com.google.common.annotations.VisibleForTesting;
+import org.junit.Test;
+
+public class PetStoreUserTest {
+
+    @Test
+    public void ptest()
+    {
+        System.out.println("Hello World ..!!! ");
+    }
+}
